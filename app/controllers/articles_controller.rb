44a926f2c505b0dbd19980f15d9a7591eac7706c
jazzari <passionjazz@gmail.com>
2021-02-03 16:46:25 +0100

    Create scope 'recent' in Article model to return collection in proper order: last created, first listed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController 
 
     def index
-        articles = Article.all
+        articles = Article.recent
         json_string = ArticleSerializer.new(articles).serializable_hash.to_json
         render json: json_string 
     end
