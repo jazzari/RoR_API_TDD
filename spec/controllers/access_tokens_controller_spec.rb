5ab03220a50dccc965fd833cdf898dc54898e462
jazzari <passionjazz@gmail.com>
2021-02-09 14:58:19 +0100

    Handling invalid_token in Access_Token_Controller's create action

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,25 +2,44 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do 
     describe '#create' do 
-        subject { post :create }
-        context 'when invalid request' do 
+
+        shared_examples_for "unauthorized_requests" do 
             let(:error) do 
-                      {
-                        "status" => "401",
-                        "source" => { "pointer" => "/data/attributes/code " },
-                        "title" =>  "Authentication code is invalid",
-                        "detail" => "You must provide valid code in order to exchange it for token"
-                      }
+                {
+                  "status" => "401",
+                  "source" => { "pointer" => "/data/attributes/code " },
+                  "title" =>  "Authentication code is invalid",
+                  "detail" => "You must provide valid code in order to exchange it for token"
+                }
             end
             it 'should return 401 status code' do 
-                 subject
-                 expect(response).to have_http_status(401)
+                subject
+                expect(response).to have_http_status(401)
             end
             it 'should return the proper error body' do 
                 subject
                 expect(json['errors']).to include(error)
             end
         end
+
+        context 'when no code provided' do 
+            subject { post :create }
+            it_behaves_like "unauthorized_requests"
+        end
+
+        context 'when invalid code provided' do 
+            let(:github_error) { 
+                double("Sawyer::Resource", error: "bad_verification_code")
+            }
+
+            before do 
+                allow_any_instance_of(Octokit::Client).to receive(
+                    :exchange_code_for_token).and_return(github_error)
+            end
+            subject { post :create, params: { code: 'invalid_code' } }
+            it_behaves_like "unauthorized_requests"
+        end
+
         context 'when success request' do 
         end
     end
